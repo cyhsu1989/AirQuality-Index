2c3bed0717f0338c8759a28a26e9c50602902c32
Chin-Yu Hsu <sw9856@gmail.com>
2015-10-17 00:01:31 +0800

    Test json url

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,7 @@ app.controller('MainController', function($scope,$http,$sce){
 
 
 	$scope.taiwanCounty=['基隆市','臺北市','新北市','桃園市','新竹市','新竹縣','宜蘭縣','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','金門縣','澎湖縣','連江縣'];
-	$http.get('./data/data.json').success(function(data){
+	$http.get('http://opendata.epa.gov.tw/ws/Data/AQX/?$orderby=SiteName&$skip=0&$top=1000&format=json').success(function(data){
 		$scope.countyData = data;
 	});
 });
