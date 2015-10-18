64005ca4d3f43700badd3e501c3da045aef8a099
Chin-Yu Hsu <sw9856@gmail.com>
2015-10-19 00:54:35 +0800

    Fixed the online json API

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,19 @@ var app = angular.module('AirApp', []);
 app.controller('MainController', function($scope,$http,$sce){
 	$scope.type='area';
 	//$scope.countyData = {};
+	$scope.countyData;
 
 
 	$scope.taiwanCounty=['基隆市','臺北市','新北市','桃園市','新竹市','新竹縣','宜蘭縣','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','金門縣','澎湖縣','連江縣'];
-	$http.get('./data/data.json').success(function(data){
+
+    var url = "http://opendata.epa.gov.tw/ws/Data/AQX/?$orderby=SiteName&$skip=0&$top=1000&format=json&callback=JSON_CALLBACK";
+
+    $http.jsonp(url)
+        .success(function(data){
+            $scope.countyData = data;
+        });
+
+	/*$http.get('http://opendata.epa.gov.tw/ws/Data/AQX/?$orderby=SiteName&$skip=0&$top=1000&format=json').success(function(data){
 		$scope.countyData = data;
-	});
+	});*/
 });
