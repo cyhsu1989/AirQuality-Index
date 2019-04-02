8c48c36c5e246a8b67482887564c834334773f73
Chin-Yu Hsu <cyhsu1989@gmail.com>
2019-04-02 18:49:10 +0800

    修正沒有資料的問題及調整。
    更新取得資料的 API 位置以及資料屬性修改。
    另外調整 option 沒有選擇縣市時的顯示。

--- javascript/main.js
+++ javascript/main.js
@@ -2,20 +2,19 @@ var app = angular.module('AirApp', []);
 
 app.controller('MainController', function($scope,$http,$sce){
 	$scope.type='area';
-	//$scope.countyData = {};
-	$scope.countyData;
+	$scope.rawData = [];
+	$scope.countyData = [];
 
 
 	$scope.taiwanCounty=['基隆市','臺北市','新北市','桃園市','新竹市','新竹縣','宜蘭縣','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','金門縣','澎湖縣','連江縣'];
 
-    var url = "http://opendata.epa.gov.tw/ws/Data/AQX/?$orderby=SiteName&$skip=0&$top=1000&format=json&callback=JSON_CALLBACK";
+    var url = "https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000259?sort=SiteName&offset=0&limit=1000&callback=JSON_CALLBACK";
 
     $http.jsonp(url)
         .success(function(data){
-            $scope.countyData = data;
+            if (data.success) {
+                $scope.rawData = data.result.records;
+                $scope.countyData = $scope.rawData;
+            }
         });
-
-	/*$http.get('http://opendata.epa.gov.tw/ws/Data/AQX/?$orderby=SiteName&$skip=0&$top=1000&format=json').success(function(data){
-		$scope.countyData = data;
-	});*/
 });
